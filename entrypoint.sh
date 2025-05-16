0616e5a905dc9cfdd1c9dd8065035740f235f774
Stefan Dlugolinsky <stefan.dlugolinsky@savba.sk>
2025-05-16 09:45:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-printenv > /tmp/env_vars.sh
-sed -i -E 's/NOMAD_META_owner_name=(.+)/NOMAD_META_owner_name="\1"/g' /tmp/env_vars.sh
+# retrieve env vars and quote values containing space
+printenv | while IFS='=' read -r k v; do [[ "$v" == *[[:space:]]* ]] && echo "$k=\"$v\"" || echo "$k=$v"; done > /tmp/env_vars.sh
 cron && tail -f /var/log/cron.log
